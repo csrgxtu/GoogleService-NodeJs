02d8c63997aa5fe78afd52f43d581c73c9aa2255
Archer Reilly <csrgxtu0@gmail.com>
2015-02-09 20:39:58 +0800

    JSON in Javascript with quote isnt work that well

--- test/GoogleSearchResult.js
+++ test/GoogleSearchResult.js
@@ -9,21 +9,37 @@ request(url, function(error, response, html) {
     var title = $('title').text();
     var stats = $('div[id=resultStats]').text();
     var statsTime = $('div[id=resultStats]>nobr').text();
-    var recs = {};
+    var recs = [];
     $('li[class=g]').each(function(i, element) {
       var title = $(this).children().eq(0).text();
       var url = $(this).children().eq(0).find('a').attr('href');
       url = url.slice(7, url.indexOf('&sa'));
-      var org = $(this).children().eq(1).html();
-      console.log('DEBUG: ' + org);
+      var abs = $(this).find('span[class=st]').text();
+      var tmp = {
+        title: title,
+        url: url,
+        abs: abs
+      };
+      recs.push(tmp);
     });
+    
+    var relatedKeywords = [];
+    $('p[class=_Bmc]').each(function(i, element) {
+      var tmp = {
+        keyWord: $(this).text().replace("'", "\'").replace('"', '\"')
+      };
+      relatedKeywords.push(tmp);
+    });
+
     var data = {
       title: title,
       stats: stats,
       statsTime: statsTime,
-      recs: {},
+      relatedKeywords: relatedKeywords,
+      recs: recs,
     };
-    //console.log(res);
+
+    console.log(data);
   }
 });
 console.log('Started ...');
